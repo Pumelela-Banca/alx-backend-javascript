b9e37440c8aae847fe6f387a172af97b5b8bd590
Pumelela-Banca <pumelelabanca@gmail.com>
2024-01-06 17:39:50 +0200

    added semicolon

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,5 +1,5 @@
 import Classroom from './0-classroom';
 
 export default function () {
-  return [new Classroom(19), new Classroom(20), new Classroom(34)]
+  return [new Classroom(19), new Classroom(20), new Classroom(34)];
 }

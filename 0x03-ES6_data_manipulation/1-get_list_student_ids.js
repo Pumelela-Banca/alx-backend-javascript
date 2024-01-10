6749d68a666486067914a3594b02766b51ea4a68
Pumelela-Banca <pumelelabanca@gmail.com>
2024-01-10 02:57:11 +0200

    makes calls to  api

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,10 @@
+export default function getListStudentIds(arr) {
+  if (arr.isArray() === false) {
+    return [];
+  } else {
+    const newArray = [];
+    for (let ind in arr) {
+      
+    }
+  }
+}

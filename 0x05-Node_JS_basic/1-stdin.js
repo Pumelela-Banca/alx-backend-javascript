5835823674466d7af1426e274d7298e0a2962455
Pumelela-Banca <pumelelabanca@gmail.com>
2024-02-27 17:57:41 +0200

    added lines to print

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,15 +1,17 @@
-const rdln = require('readline');
-
-const reader = rdln.createInterFace({
+const readline = require("readline");
+const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout,
+  output: process.stdout
 });
 
 console.log('Welcome to Holberton School, what is your name?');
 
-reader.on('line', (name) => {
+rl.question("", function(name) {
   console.log(`Your name is: ${name}`);
-  reader.close();
-}).on('close', () => {
-  console.log('This important software is now closing');
+  rl.close();
+});
+
+rl.on("close", function() {
+  console.log("This important software is now closing");
+  process.exit(0);
 });

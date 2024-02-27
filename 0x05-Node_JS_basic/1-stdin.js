3d049b66eff056461d6cea3652cdf00053943bf5
Pumelela-Banca <pumelelabanca@gmail.com>
2024-02-27 18:16:12 +0200

    uses annonimus functions

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,18 +1,18 @@
 const readline = require('readline');
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
 console.log('Welcome to Holberton School, what is your name?');
 
-rl.question('', function (name) {
+rl.question('', (name) => {
   if (name) {
     console.log(`Your name is: ${name}`);
   }
   rl.close();
 });
 
-rl.on('close', function () {
+rl.on('close', () => {
   console.log('This important software is now closing');
 });

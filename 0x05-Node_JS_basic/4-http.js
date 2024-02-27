e9dd2b0a5cb83c60d0fa17258b5adb32a4d9aaa2
Pumelela-Banca <pumelelabanca@gmail.com>
2024-02-27 23:46:39 +0200

    added write

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,14 +1,21 @@
 const http = require('http');
 
+const hostname = 'localhost';
 const port = 1245;
 
-const app = http.createServer((_, res) => {
+const app = http.createServer();
+
+
+app.on('request', (_, res) => {
+  const responseText = 'Hello Holberton School!';
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Holberton School!\n')
+  res.write(Buffer.from(responseText));
 });
 
 
-app.listen(port);
+app.listen(port, hostname, () => {
+  process.stdout.write(`Server listening at -> http://${hostname}:${port}\n`);
+});
 
 module.exports = app;

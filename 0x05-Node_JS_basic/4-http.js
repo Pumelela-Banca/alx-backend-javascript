51685ab46c8b1d8bd458dea8be457350d18c652c
Pumelela-Banca <pumelelabanca@gmail.com>
2024-02-27 23:49:37 +0200

    went ack to origional

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,21 +1,11 @@
 const http = require('http');
 
-const hostname = 'localhost';
-const port = 1245;
-
-const app = http.createServer();
-
-
-app.on('request', (_, res) => {
-  const responseText = 'Hello Holberton School!';
+const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.write(Buffer.from(responseText));
+  res.end('Hello Holberton School!\n');
 });
 
-
-app.listen(port, hostname, () => {
-  process.stdout.write(`Server listening at -> http://${hostname}:${port}\n`);
-});
+app.listen(1245);
 
 module.exports = app;

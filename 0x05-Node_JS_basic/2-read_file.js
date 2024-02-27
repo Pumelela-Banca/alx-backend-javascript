d0837c4c855c57bc9ea50c58d1c4506928577544
Pumelela-Banca <pumelelabanca@gmail.com>
2024-02-27 18:26:17 +0200

    module that counts the number of students

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const data = fs.readFileSync(path, 'utf8');
+    const lines = data.split('\n');
+    const students = lines.slice(1).filter(line => line !== '');
+    console.log(`Number of students: ${students.length}`);
+    const fields = {};
+    for (const student of students) {
+      const field = student.split(',')[3];
+      if (!fields[field]) {
+        fields[field] = [];
+      }
+      fields[field].push(student.split(',')[0]);
+    }
+    for (const field in fields) {
+      console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+    }
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;

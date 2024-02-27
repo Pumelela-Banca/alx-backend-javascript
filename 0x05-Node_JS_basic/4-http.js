6d5f86ac3fa45548bd42ce2200532487f28ccbb2
Pumelela-Banca <pumelelabanca@gmail.com>
2024-02-27 23:38:55 +0200

    uses res.ebd

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -6,8 +6,9 @@ const port = 1245;
 const app = http.createServer((_, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.write(Buffer.from('Hello Holberton School!'));
+  res.end('Hello Holberton School!\n')
 });
+
 
 app.listen(port, hostname, () => {
   process.stdout.write(`Server listening at -> http://${hostname}:${port}\n`);

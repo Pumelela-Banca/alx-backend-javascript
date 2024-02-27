302237ac677f2f14f5afc39638f8ac23335c0f13
Pumelela-Banca <pumelelabanca@gmail.com>
2024-02-27 18:12:46 +0200

    fixed semistandard checks

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,18 +1,18 @@
-const readline = require("readline");
+const readline = require('readline');
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout,
+  output: process.stdout
 });
 
 console.log('Welcome to Holberton School, what is your name?');
 
-rl.question("", function(name) {
+rl.question('', function (name) {
   if (name) {
     console.log(`Your name is: ${name}`);
   }
   rl.close();
 });
 
-rl.on("close", function() {
-  console.log("This important software is now closing");
+rl.on('close', function () {
+  console.log('This important software is now closing');
 });

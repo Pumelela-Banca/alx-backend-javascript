bc4ad7b11cb63b4cda47090f6edea663b06935c3
Pumelela-Banca <pumelelabanca@gmail.com>
2024-03-01 01:06:00 +0200

    added tests for floats

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,26 +1,25 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
-describe('calculateNumber', () => {
-  it('adds two numbers (positive integers)', () => {
-    assert.strictEqual(calculateNumber(2.0, 2.0), 4);
-  });
+describe('test for calculateNumber', () => {
+    it('should get the sum of integers', () => {
+      assert.strictEqual(calculateNumber(1, 1), 2);
+      assert.strictEqual(calculateNumber(1, 2), 3);
+      assert.strictEqual(calculateNumber(3, -2), 1);
+      assert.strictEqual(calculateNumber(-9, 4), -5);
+    });
   
-  it('adds two numbers (positive decimals)', () => {
-    assert.strictEqual(calculateNumber(2.3, 1.8), 4);
+    it('should round floats and calulate the sum', () => {
+      assert.strictEqual(calculateNumber(1.2, 4.4), 5);
+      assert.strictEqual(calculateNumber(4, 8.8), 13);
+      assert.strictEqual(calculateNumber(-3.5, 5), 2);
+      assert.strictEqual(calculateNumber(1, 3.8), 5);
+      assert.strictEqual(calculateNumber(2.5, 4.6), 8);
+      assert.strictEqual(calculateNumber(-5.9, -6), -12);
+    });
+  
+    it('should check arguments',  () => {
+      assert.strictEqual(calculateNumber(), NaN);
+      assert.strictEqual(calculateNumber(3), NaN);
+    });
   });
-  it('adds two numbers (negative integers)', () => {
-    assert.strictEqual(calculateNumber(-2.0, -2.0), -4);
-  });
-  it('adds two floats (negative and positive)', () => {
-    assert.strictEqual(calculateNumber(-2.0, 2.0), 0);
-    assert.strictEqual(calculateNumber(1, 3.8), 5);
-    assert.strictEqual(calculateNumber(2.5, 4.6), 8);
-    assert.strictEqual(calculateNumber(-5.9, -6), -12);
-  });
-
-  it('should check arguments',  () => {
-    assert.strictEqual(calculateNumber(), NaN);
-    assert.strictEqual(calculateNumber(3), NaN);
-  });
-});

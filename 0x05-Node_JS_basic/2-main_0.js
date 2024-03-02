8890c5f74c443e76653e2c2f7bdc4ab8c4a4ccdb
Pumelela-Banca <pumelelabanca@gmail.com>
2024-03-02 20:12:31 +0200

    file excecution fixes

--- 0x05-Node_JS_basic/2-main_0.js
+++ 0x05-Node_JS_basic/2-main_0.js
@@ -13,12 +13,8 @@ describe('countStudents', () => {
   afterEach(() => {
     consoleSpy.restore();
   });
-
-  it('logs to the console the right messages', () => {
-    countStudents('./database.csv');
-
-    expect(consoleSpy.calledWith('Number of students: 10')).to.be.true;
-    expect(consoleSpy.calledWith('Number of students in CS: 6. List: Johenn, Arielle, Jonathen, Emmenuel, Guillaume, Katie')).to.be.true;
-    expect(consoleSpy.calledWith('Number of students in SWE: 4. List: Guillaume, Joseph, Paul, Tommy')).to.be.true;
+  
+  it('throws the correct error message', () => {
+    expect(() => countStudents('./blabl.csv')).to.throw('Cannot load the database');
   });
 });
